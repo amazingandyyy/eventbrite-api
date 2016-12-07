a1088969574abc3ce1bce964abbed8cb3a0b3f66
amazingandyyy <amazingandyyy@gmail.com>
2016-12-06 17:54:58 -0800

    clean up
    clean up

--- index.js
+++ index.js
@@ -10,24 +10,26 @@ module.exports = {
         var events, clean_res;
         filter.token = publicKey
         var api_uri = eb_event_search_uri + '?' + qs.stringify(filter);
-        if (!dev_mode){
-          request(api_uri, function(err, res) {
-             events = JSON.parse(res.body).events;
-             clean_res = events.map(function(event) {
-                 return {
-                     name: event.name.text,
-                     id: event.id,
-                     url: event.url,
-                     start: event.start.local,
-                     // end: event.end,
-                     thumbnail: event.logo ? event.logo.url : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png'
-                 }
-             })
-             cb(err, res, clean_res);
-         })
-       }else{
-         console.log('development mode on')
-         cb(null, {body: 'developed body'}, 'events');
-       }
+        if (!dev_mode) {
+            request(api_uri, function(err, res) {
+                events = JSON.parse(res.body).events;
+                clean_res = events.map(function(event) {
+                    return {
+                        name: event.name.text,
+                        id: event.id,
+                        url: event.url,
+                        start: event.start.local,
+                        // end: event.end,
+                        thumbnail: event.logo ? event.logo.url : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png'
+                    }
+                })
+                cb(err, res, clean_res);
+            })
+        } else {
+            console.log('development mode on')
+            cb(null, {
+                body: 'developed body'
+            }, 'events');
+        }
     }
 }
