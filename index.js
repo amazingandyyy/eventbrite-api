fb5373f0a5c7f03b6adaac6589c7f05d2374be05
amazingandyyy <amazingandyyy@gmail.com>
2016-12-06 17:54:18 -0800

    add qs
    add qs

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 var request = require('request');
+var qs = require('querystring');
+
 var publicKey = 'R5ZWGGH5QB65L3MAW62S';
 var eb_event_search_uri = 'https://www.eventbriteapi.com/v3/events/search/';
+var dev_mode = false;
 
 module.exports = {
     getAll: function(filter, cb) {
-        var result, events, filter_uri = '',
-            combine;
-        for (var key in filter) {
-            combine = key + '=' + filter[key].split(' ').join('+')
-            filter_uri = filter_uri + combine + '&'
-        }
-        var api_uri = eb_event_search_uri + '?' + filter_uri + 'token=' + publicKey;
-        request(api_uri, function(err, res) {
-            result = JSON.parse(res.body).events;
-            events = result.map(function(event) {
-                return {
-                    name: event.name.text,
-                    id: event.id,
-                    url: event.url,
-                    start: event.start.local,
-                    // end: event.end,
-                    thumbnail: event.logo ? event.logo.url : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png'
-                }
-            })
-            cb(err, res, events);
-        })
+        var events, clean_res;
+        filter.token = publicKey
+        var api_uri = eb_event_search_uri + '?' + qs.stringify(filter);
+        if (!dev_mode){
+          request(api_uri, function(err, res) {
+             events = JSON.parse(res.body).events;
+             clean_res = events.map(function(event) {
+                 return {
+                     name: event.name.text,
+                     id: event.id,
+                     url: event.url,
+                     start: event.start.local,
+                     // end: event.end,
+                     thumbnail: event.logo ? event.logo.url : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png'
+                 }
+             })
+             cb(err, res, clean_res);
+         })
+       }else{
+         console.log('development mode on')
+         cb(null, {body: 'developed body'}, 'events');
+       }
     }
 }
